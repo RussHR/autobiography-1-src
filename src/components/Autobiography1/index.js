881de2803d951bfe0f7442d88a4545c1c32dda25
RussHR <russ.rinzler@gmail.com>
2016-07-31 18:50:07 -0700

    add react to the application

---
+++ src/components/Autobiography1/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Autobiography1 extends Component {
+    render() {
+        return (
+            <div>
+                I am the app!
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ if (module.hot) {
     module.hot.accept();
 }
 
+import React from 'react';
+import { render } from 'react-dom';
+import Autobiography1 from './components/Autobiography1';
 import 'normalize.css';
 import './main.scss';
 
-console.log('hay I started');
+render(<Autobiography1 />, document.getElementById('autobiography-1'));
